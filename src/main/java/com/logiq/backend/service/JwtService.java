22846697f3293658c6be3f42ebee17105068210d
shan2003034 <shangajanayake7@gmail.com>
2026-07-14 21:18:12 +0530

    feat(UserAuth): update User Auth process and email verification process

--- src/main/java/com/logiq/backend/service/JwtService.java
+++ src/main/java/com/logiq/backend/service/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.security.Key;
 import java.util.Date;
@@ -18,7 +19,8 @@ import java.util.function.Function;
 public class JwtService {
 
 
-    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    private String SECRET_KEY;
+
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
