a77e803528c82c88ae1ed64d4eb182c6c1be73b9
shan2003034 <shangajanayake7@gmail.com>
2026-07-14 12:24:45 +0530

    Initial commit

---
+++ src/main/java/com/logiq/backend/LogiqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.logiq.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiqBackendApplication.class, args);
+	}
+
+}
