47560a68a6fb8a96f242bc595a8b8d76a4e20874
shan2003034 <shangajanayake7@gmail.com>
2026-07-20 11:17:43 +0530

    feat(Project Collaborators) develop add project collaborator and send invite email

--- src/main/java/com/logiq/backend/model/User.java
+++ src/main/java/com/logiq/backend/model/User.java
@@ -52,6 +52,9 @@ public class User implements UserDetails{
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Project> projects;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ProjectCollaborator> collaborativeProjects;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
