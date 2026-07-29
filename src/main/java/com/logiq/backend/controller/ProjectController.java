0c80d87f66869d20196c502d14a34b270ec39274
shan2003034 <shangajanayake7@gmail.com>
2026-07-29 18:10:14 +0530

    feat(Project setting & User profile); Update project setting & User profile process

--- src/main/java/com/logiq/backend/controller/ProjectController.java
+++ src/main/java/com/logiq/backend/controller/ProjectController.java
@@ -48,7 +48,7 @@ public class ProjectController {
 
     @GetMapping("/shared")
     public ResponseEntity<List<ProjectResponse>> getSharedProjects(Principal principal) {
-        // Principal හරහා දැනට ලොග් වී සිටින User ගේ Email එක ලබා ගනී (JWT Token එකෙන්)
+
         String userEmail = principal.getName();
 
         List<ProjectResponse> sharedProjects = projectService.getSharedProjectsForUser(userEmail);
